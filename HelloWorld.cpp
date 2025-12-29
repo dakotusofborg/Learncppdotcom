7bdaf2ce7501db5557fc8a6d2427fdb717144f29
Dakota Cahill <dakota.cahill@outlook.com>
2025-12-29 14:07:05 -0500

    Refactor to main(), add console output examples
    Converted foo(int) to a main() function for a runnable program. Added examples of printing literals to the console with std::cout. Declared int and double variables. Removed unused foo function and its invocation.

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,14 +1,15 @@
-#include <iostream>
-
-static void foo(int)
-{
-	// ... 
-}
+#include <iostream> // for std::cout 
 
 int main()
 {
-	unsigned int x{ 5 };
-	foo(x);
+	std::cout << 5; // print the literal 5 to the console
+	std::cout << -6.7; // print the literal -6.7 to the console
+	std::cout << 'H'; // print the literall character 'H' to the console
+	std::cout << "Hello"; // print the literal string "Hello" to the console
 
-	return 0;
+	int x; // define a variable name x (of type int) 
+
+	double width; // define a variable name width (of type double)
+
+	return 0; // indicate that the program ended successfully
 }
