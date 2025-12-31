a4911bf96c6eaa58fb2726aa17c4f116b6ce3572
Dakota Cahill <dakota.cahill@outlook.com>
2025-12-31 15:25:13 -0500

    Refactor main.cpp for basic I/O and variable usage
    Removed unused math/physics constants and related output. Added greeting messages, variable initialization, and user input prompts for two and three numbers. Updated output formatting to use both std::endl and newline characters. Focus is now on demonstrating basic input/output and variable handling.

--- LearnCpp_Current/main.cpp
+++ LearnCpp_Current/main.cpp
@@ -2,15 +2,36 @@
 
 int main()
 {
-	// here's some math/physics values that we copy-paste from elsewhere
-	[[maybe_unused]] double pi{ 3.14159265358979323846 };
-	[[maybe_unused]] double gravity{ 9.80665 }; // m/s^2
-	[[maybe_unused]] double phi{ 1.618033988749895 }; // golden ratio
+	std::cout << "Hi!" << std::endl; // std::endl will cause the cursor to move to the next line
+	std::cout << "My name is Dakota" << std::endl; 
 
-	std::cout << pi << '\n';// pi is used 
-	std::cout << phi << '\n';// phi is used
+	int x{ 5 }; // define integer variable x and initialize it to 5
 
-	// the compiler will likely complain that gravity is being defined but unused 
+	std::cout << x << std::endl; // print the value of x to the console 
+
+	std::cout << "Hello" << " World!\n"; // print "Hello World!" to the console
+
+	std::cout << "x is equal: " << x << '\n'; // print "x is equal: 5" to the console single quotes for newline character (conventional) 
+	std::cout << "Yep." << "\n"; // doubel quoted (by itself) (unconventional but ok) 
+	std::cout << "And that's all, folks.\n"; // between double quotes in existing text (also conventional)
+
+	std::cout << "Enter two numbers separated by a space: "; // ask the user for a number
+
+	int y{}; // define variable x tro hold the user input (and value-initialize it) 
+	int z{}; // define variable z to hold another user input (and value-initialize it)
+
+	std::cin >> y >> z; // get two numbers and store in variable y and z respectively
+
+	std::cout << "You entered: " << y << " and " << z << '\n';
+
+	int a{};
+	int b{};
+	int c{};
+	
+	std::cout << "Enter three numbers separated by spaces: "; // ask the user for three numbers
+	std::cin >> a >> b >> c; // get three numbers and store in variable a, b, and c respectively
+	
+	std::cout << "You entered: " << a << b << c << '\n'; // get three numbers and store in variable a, b, and c respectively 
 
 	return 0; 
 }
